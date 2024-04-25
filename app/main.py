4f865fcd75678fa94a30a768afbd72a2f6af76c1
ItsNikolor <itsnikolor@gmail.com>
2024-04-25 16:57:05 +0300

    empty

--- app/main.py
+++ app/main.py
@@ -9,18 +9,18 @@ logger.setLevel(logging.INFO)
 def main():
     print("Logs from your program will appear here!")
 
-    # with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
     with socket.create_server(("localhost", 4221), reuse_port=True) as server_socket:
         conn, addr = server_socket.accept()
         with conn:
             print(f"Connected by {addr}")
-            # while True:
-            data = conn.recv(1024)
-            # if not data:
-            #     break
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    break
             conn.sendall('HTTP/1.1 200 OK\r\n\r\n')
+            
         # sleep(3)
-        
+
 
 
 
