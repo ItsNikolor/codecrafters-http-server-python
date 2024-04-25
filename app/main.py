e0348b20fdae89194c78de8aa037c144047900d3
ItsNikolor <itsnikolor@gmail.com>
2024-04-25 16:39:54 +0300

    stage 2

--- app/main.py
+++ app/main.py
@@ -1,4 +1,3 @@
-# Uncomment this to pass the first stage
 import socket
 
 
@@ -6,9 +5,28 @@ def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
 
-    # Uncomment this to pass the first stage
-    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
+        server_socket.bind(("localhost", 4221))
+        server_socket.listen()
+        server_socket.accept()
+
+        conn, addr = server_socket.accept()
+        with conn:
+            print(f"Connected by {addr}")
+            # while True:
+            data = conn.recv(1024)
+            # if not data:
+            #     break
+            conn.sendall('HTTP/1.1 200 OK\r\n\r\n')
+
+
+
+    # server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
+    # server_socket.accept() # wait for client
+
+    
+
+    # server_socket.close()
 
 
 if __name__ == "__main__":
