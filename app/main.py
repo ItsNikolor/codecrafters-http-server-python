5b9d6d0060a2ae79c6c6bb9bde3367ae20c6ce2f
ItsNikolor <itsnikolor@gmail.com>
2024-04-25 16:54:45 +0300

    empty

--- app/main.py
+++ app/main.py
@@ -9,23 +9,9 @@ logger.setLevel(logging.INFO)
 def main():
     print("Logs from your program will appear here!")
 
-
-    logger.info('Im here')
-    print('Im here')
-
     # with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
     with socket.create_server(("localhost", 4221), reuse_port=True) as server_socket:
-        logger.info('Im here')
-        print('Im here')
-
-        # server_socket.bind(("localhost", 4221))
-        # server_socket.listen()
-        server_socket.accept()
-
-        logger.info('Im here')
-        print('Im here')
         conn, addr = server_socket.accept()
-        logger.info('Connected by {}'.format(addr))
         with conn:
             print(f"Connected by {addr}")
             # while True:
@@ -33,7 +19,8 @@ def main():
             # if not data:
             #     break
             conn.sendall('HTTP/1.1 200 OK\r\n\r\n')
-        sleep(3)
+        # sleep(3)
+        
 
 
 
