10cb30dc7bc4156d12e0836895bbd95513dd2cca
ItsNikolor <itsnikolor@gmail.com>
2024-04-25 16:52:58 +0300

    empty

--- app/main.py
+++ app/main.py
@@ -1,17 +1,31 @@
 import socket
 from time import sleep
 
+import logging
+logger = logging.getLogger(__name__)
+logger.basicConfig(level=logging.INFO)
+
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
 
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
-        server_socket.bind(("localhost", 4221))
-        server_socket.listen()
+
+    logger.info('Im here')
+    print('Im here')
+
+    # with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
+    with socket.create_server(("localhost", 4221), reuse_port=True) as server_socket:
+        logger.info('Im here')
+        print('Im here')
+
+        # server_socket.bind(("localhost", 4221))
+        # server_socket.listen()
         server_socket.accept()
 
+        logger.info('Im here')
+        print('Im here')
         conn, addr = server_socket.accept()
+        logger.info('Connected by {}'.format(addr))
         with conn:
             print(f"Connected by {addr}")
             # while True:
@@ -21,14 +35,6 @@ def main():
             conn.sendall('HTTP/1.1 200 OK\r\n\r\n')
         sleep(3)
 
-
-
-    # server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    # server_socket.accept() # wait for client
-
-    
-
-    # server_socket.close()
 
 
 if __name__ == "__main__":
